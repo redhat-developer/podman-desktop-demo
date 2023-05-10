62afb59ef36bcf7839cd292bf8abe923189b9b46
Praveen Kumar <kumarpraveen.nitdgp@gmail.com>
2023-05-10 15:23:03 +0530

    Pod: Change the demo to use centos-httpd image instead nginx
    nginx image doesn't work by default on openshift because it run
    as `root` user and you will get `CreateContainerConfigError`.
    
    ```
    $ oc get pods
    NAME       READY   STATUS                       RESTARTS   AGE
    test-pod   0/1     CreateContainerConfigError   0          19s
    
    $ oc get pod test-pod -oyaml
    [...]
        state:
          waiting:
            message: 'container has runAsNonRoot and image will run as root (pod: "test-pod_default(10f4ec1d-329c-4a01-8a88-c0fb545b553f)",
              container: test)'
            reason: CreateContainerConfigError
    ```

--- pods/start-pods.sh
+++ pods/start-pods.sh
@@ -5,8 +5,8 @@
 podman pod create --name simple-pod
 
 # Create a Pod with Volume
-podman pod create --name pod-with-volume -p 8000:80 -v demo-earthly:/usr/share/nginx/html
+podman pod create --name pod-with-volume -p 8080:8080 -v demo-earthly:/www/html
 
 # Start NGINX Pod from YAML
-podman play kube ./pods/nginx-pod.yaml
+podman play kube ./pods/httpd-pod.yaml
 
