5ac1492c30d570bf78ff88e48183615312649ac9
Jeff MAURY <jmaury@redhat.com>
2023-05-15 17:10:10 +0200

    Update pods/start-pods.sh

--- pods/start-pods.sh
+++ pods/start-pods.sh
@@ -7,6 +7,6 @@ podman pod create --name simple-pod
 # Create a Pod with Volume
 podman pod create --name pod-with-volume -p 8080:8080 -v demo-earthly:/www/html
 
-# Start NGINX Pod from YAML
+# Start HTTP Pod from YAML
 podman play kube ./pods/httpd-pod.yaml
 
