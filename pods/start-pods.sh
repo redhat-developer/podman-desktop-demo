53abf7a58ab44f64776ca2fe20bff9b8f03e4359
Stévan Le Meur <1636769+slemeur@users.noreply.github.com>
2023-05-18 17:14:29 +0200

    Update script to start pods with podman (#9)
    * update readme for podify demo
    
    Signed-off-by: Stévan Le Meur <1636769+slemeur@users.noreply.github.com>
    
    * add start of the pods
    
    Signed-off-by: Stévan Le Meur <1636769+slemeur@users.noreply.github.com>
    
    * change ports used for httpd pod
    
    Signed-off-by: Stévan Le Meur <1636769+slemeur@users.noreply.github.com>
    
    * update port used by redis-stack
    
    Signed-off-by: Stévan Le Meur <1636769+slemeur@users.noreply.github.com>
    
    * use different port for python-app
    
    Signed-off-by: Stévan Le Meur <1636769+slemeur@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Stévan Le Meur <1636769+slemeur@users.noreply.github.com>

--- pods/start-pods.sh
+++ pods/start-pods.sh
@@ -3,9 +3,11 @@
 
 # Create a Simple Pod
 podman pod create --name simple-pod
+podman pod start simple-pod
 
 # Create a Pod with Volume
-podman pod create --name pod-with-volume -p 8080:8080 -v demo-earthly:/www/html
+podman pod create --name pod-with-volume -p 8099:8080 -v demo-earthly:/www/html
+podman pod start pod-with-volume
 
 # Start HTTP Pod from YAML
 podman play kube ./pods/httpd-pod.yaml
