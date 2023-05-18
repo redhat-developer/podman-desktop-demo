2c46852306dd23e29f3d2f62e4f6af8824961ab0
Stévan Le Meur <1636769+slemeur@users.noreply.github.com>
2023-05-18 16:13:18 +0200

    update port used by redis-stack
    Signed-off-by: Stévan Le Meur <1636769+slemeur@users.noreply.github.com>

--- run-containers.sh
+++ run-containers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run of Redis Stack
-podman run -d --name redis-stack -p 6379:6379 -p 8001:8001 docker.io/redis/redis-stack:latest
+podman run -d --name redis-stack -p 6380:6379 -p 8001:8001 docker.io/redis/redis-stack:latest
 
 # Run of HTTPD
 podman run -dt -p 8082:80/tcp docker.io/library/httpd
