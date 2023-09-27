60c64d0d0213009a0a8166e782400be1f72adc72
Máirín Duffy <duffy@redhat.com>
2023-09-27 11:35:35 -0400

    fix: adding networking fix to primary podify demo
    demo works now :)
    
    Signed-off-by: Máirín Duffy <duffy@redhat.com>

--- primary-podify-demo/preload.sh
+++ primary-podify-demo/preload.sh
@@ -1,4 +1,5 @@
 #bash 
-podman run -d -p 6379:6379 --name redis quay.io/centos7/redis-5-centos7
-podman run -d -p 8088:5000 --add-host=redis:$(podman inspect redis | jq -r '.[0].NetworkSettings.IPAddress') --name python-app quay.io/slemeur/python-app
+podman network create podify
+podman run -d -p 6379:6379 --net podify --name redis quay.io/centos7/redis-5-centos7
+podman run -d -p 8088:5000 --net podify --add-host=redis:$(podman inspect redis | jq -r '.[0].NetworkSettings.IPAddress') --name python-app quay.io/slemeur/python-app
 
