c3758d9394af9e6e4025a872bf0fe1cbe23aa8c0
Florent Benoit <fbenoit@redhat.com>
2023-10-03 16:43:58 +0200

    chore: update
    use new image name
    also checks if resources do not exists before proceeding

--- primary-podify-demo/preload.sh
+++ primary-podify-demo/preload.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-podman network create podify
-podman run -d -p 6379:6379 --net podify --name redis quay.io/centos7/redis-5-centos7
-podman run -d -p 8088:5000 --net podify --name python-app quay.io/slemeur/python-app
+
+if ! podman network exists podify; then
+  podman network create podify
+fi
+
+if podman container exists redis; then
+  podman rm -f redis
+fi
+podman run -d -p 6379:6379 --net podify --name redis quay.io/podman-desktop-demo/podify-demo-backend:v1
+
+if podman container exists python-frontend; then
+  podman rm -f python-frontend
+fi
+podman run -d -p 8088:5000 --net podify --name python-frontend quay.io/podman-desktop-demo/podify-demo-frontend:v1
